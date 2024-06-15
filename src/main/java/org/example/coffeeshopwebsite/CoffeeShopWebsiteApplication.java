20c43e3f224f42dd87e3a1fc54abd57ec7189fc6
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-15 18:18:09 +0700

    feat: Set up the necessary configuration for the project.

---
+++ src/main/java/org/example/coffeeshopwebsite/CoffeeShopWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.coffeeshopwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShopWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeShopWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/coffeeshopwebsite/CoffeeShopWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coffeeshopwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
