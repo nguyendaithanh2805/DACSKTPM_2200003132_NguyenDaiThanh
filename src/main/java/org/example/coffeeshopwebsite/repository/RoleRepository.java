4a91a6d2f6990dad83bede50b651b6ab9a2aac2d
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-19 13:38:33 +0700

    feat: Create repositories for Role, User, UserRole for security.

--- src/main/java/org/example/coffeeshopwebsite/repository/RoleRepository.java
+++ src/main/java/org/example/coffeeshopwebsite/repository/RoleRepository.java
@@ -1,9 +1,10 @@
 package org.example.coffeeshopwebsite.repository;
 
-import org.example.coffeeshopwebsite.model.Admin;
+import org.example.coffeeshopwebsite.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
--- src/main/java/org/example/coffeeshopwebsite/repository/UserRepository.java
+++ src/main/java/org/example/coffeeshopwebsite/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package org.example.coffeeshopwebsite.repository;
 
-import org.example.coffeeshopwebsite.model.Account;
+import org.example.coffeeshopwebsite.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
