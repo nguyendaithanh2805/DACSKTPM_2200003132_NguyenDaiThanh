2cc80404bf816602c6624be5921c524b346a88e4
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-19 13:36:29 +0700

    feat: Create new controller for the security.

---
+++ src/main/java/org/example/coffeeshopwebsite/controller/admin/RegisterController.java
@@ -0,0 +1,36 @@
+package org.example.coffeeshopwebsite.controller.admin;
+
+import org.example.coffeeshopwebsite.model.Role;
+import org.example.coffeeshopwebsite.model.User;
+import org.example.coffeeshopwebsite.model.UserRole;
+import org.example.coffeeshopwebsite.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class RegisterController {
+    private final RegisterService registerService;
+
+    @Autowired
+    public RegisterController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @GetMapping("/register")
+    public String createAccount(Model model) {
+        model.addAttribute("user", new User());
+        return "admin/register";
+    }
+
+    @PostMapping("/register-save")
+    public String saveAccount(@ModelAttribute("user") User user) {
+        registerService.saveRegister(user);
+        return "redirect:/admin/login";
+    }
+}
