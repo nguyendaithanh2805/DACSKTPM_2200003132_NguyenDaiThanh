14bc9a6370d40a848afec32b47db042a5dba2b6b
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-21 00:45:15 +0700

    delete: Deleted RegisterService and RegisterServiceImpl.

--- src/main/java/org/example/coffeeshopwebsite/service/RegisterService.java
+++
@@ -1,9 +0,0 @@
-package org.example.coffeeshopwebsite.service;
-
-import org.example.coffeeshopwebsite.model.Role;
-import org.example.coffeeshopwebsite.model.User;
-import org.example.coffeeshopwebsite.model.UserRole;
-
-public interface RegisterService {
-    void saveRegister(User user);
-}
