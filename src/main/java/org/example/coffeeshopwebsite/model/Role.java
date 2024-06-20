73f69f1ab79acb563b8658caef8df8f2ec653060
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-21 00:37:56 +0700

    chore: Update the Role entity - Removed the ID auto-increment attribute when creat the primary key to avoid 'Duplicate entry 'USER' for key 'tbl_role.UKb8pdcpsskpvgwwxu1mey2x6dq' error.

--- src/main/java/org/example/coffeeshopwebsite/model/Role.java
+++ src/main/java/org/example/coffeeshopwebsite/model/Role.java
@@ -9,10 +9,9 @@ import java.util.Set;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "role")
