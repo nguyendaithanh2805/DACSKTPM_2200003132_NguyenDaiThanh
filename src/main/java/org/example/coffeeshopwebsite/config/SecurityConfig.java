c641fae909cfe893ba9c7330b13a064ed41ca637
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-23 23:31:14 +0700

    chore: Update HttpSessionSecurityContextRepository method for session storage.

--- src/main/java/org/example/coffeeshopwebsite/config/SecurityConfig.java
+++ src/main/java/org/example/coffeeshopwebsite/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -27,10 +29,16 @@ public class SecurityConfig {
             response.sendRedirect("/access-denied");
         };
     }
+
+    @Bean
+    public HttpSessionSecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/login", "/admin/register-save", "/admin/register", "/admin/css/**", "/admin/js/**", "/admin/images/**", "/user/css/**", "/user/js/**", "/user/images/**", "/home", "/menu", "/about", "/blog", "/contact").permitAll()
                         .requestMatchers("/admin", "/admin/**"/*, "/admin/css/**", "/admin/js/**", "/admin/images/**"*/).hasAuthority("ADMIN")
@@ -48,7 +56,10 @@ public class SecurityConfig {
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/admin/login")
                 )
-                .exceptionHandling(exception -> exception.accessDeniedHandler(accessDeniedHandler()));
+                .exceptionHandling(exception -> exception.accessDeniedHandler(accessDeniedHandler()))
+                .securityContext(securityContext -> securityContext
+                        .securityContextRepository(securityContextRepository())
+                );
         return httpSecurity.build();
     }
 }
