35ae58e0ab6ca62f3ace940c78a6931e63b9245d
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-24 18:06:25 +0700

    chore: Add getCurrentUser method to fetch user exists.

--- src/main/java/org/example/coffeeshopwebsite/service/UserServiceImpl.java
+++ src/main/java/org/example/coffeeshopwebsite/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import org.example.coffeeshopwebsite.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +30,15 @@ public class UserServiceImpl implements UserService{
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         logger.info("Saved user successfully");
+    }
+
+    @Override
+    public User getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails) principal).getUsername();
+            return userRepository.findByUsername(username);
+        }
+        return null;
     }
 }
