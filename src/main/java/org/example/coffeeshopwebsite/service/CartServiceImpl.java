4126fb61dc92b5b311764d419369c1adde97f637
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-25 22:54:17 +0700

    chore: Update CartService >> >> - CartServiceImpl is responsible for storing cart, product, producCart.

--- src/main/java/org/example/coffeeshopwebsite/service/CartServiceImpl.java
+++ src/main/java/org/example/coffeeshopwebsite/service/CartServiceImpl.java
@@ -2,6 +2,7 @@ package org.example.coffeeshopwebsite.service;
 
 import org.example.coffeeshopwebsite.model.Cart;
 import org.example.coffeeshopwebsite.model.Product;
+import org.example.coffeeshopwebsite.model.ProductCart;
 import org.example.coffeeshopwebsite.model.User;
 import org.example.coffeeshopwebsite.repository.CartRepository;
 import org.example.coffeeshopwebsite.repository.ProductRepository;
@@ -18,14 +19,16 @@ import java.util.Optional;
 public class CartServiceImpl implements CartService{
     private final Logger logger = LoggerFactory.getLogger(CartServiceImpl.class);
     private final CartRepository cartRepository;
-    private final UserService userService;
     private final ProductRepository productRepository;
+    private final UserService userService;
+    private final ProductCartService productCartService;
 
     @Autowired
-    public CartServiceImpl(CartRepository cartRepository, UserService userService, ProductRepository productRepository) {
+    public CartServiceImpl(CartRepository cartRepository, UserService userService, ProductRepository productRepository, ProductCartService productCartService) {
         this.cartRepository = cartRepository;
         this.userService = userService;
         this.productRepository = productRepository;
+        this.productCartService = productCartService;
     }
 
     @Override
@@ -35,20 +38,15 @@ public class CartServiceImpl implements CartService{
         if (optionalProduct.isPresent()) {
             Product product = optionalProduct.get();
             Cart cart = new Cart();
-            cart.setProducts(new ArrayList<>());
-            cart.getProducts().add(product);
             cart.setUser(user);
             cart.setQuantity(quantity);
             cart.setTotalBill(product.getSellingPrice() * quantity);
-            if(product.getCarts() == null) {
-                product.setCarts(new ArrayList<>());
-            }
-            product.getCarts().add(cart);
             product.setQuantity(product.getQuantity() - quantity);
             cartRepository.save(cart);
             logger.info("Cart save successfully");
             productRepository.save(product);
             logger.info("Product save successfully");
+            productCartService.saveProductCart(cart, product);
         }
     }
 
