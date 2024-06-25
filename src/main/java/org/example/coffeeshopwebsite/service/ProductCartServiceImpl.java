532270bc686c6b12ac8b9e4c27e6a6b0a81ba9ce
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-25 22:55:18 +0700

    feat: Create ProductCartService and implement it.

---
+++ src/main/java/org/example/coffeeshopwebsite/service/ProductCartServiceImpl.java
@@ -0,0 +1,30 @@
+package org.example.coffeeshopwebsite.service;
+
+import org.example.coffeeshopwebsite.model.Cart;
+import org.example.coffeeshopwebsite.model.Product;
+import org.example.coffeeshopwebsite.model.ProductCart;
+import org.example.coffeeshopwebsite.repository.ProductCartRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductCartServiceImpl implements ProductCartService {
+    private final Logger logger = LoggerFactory.getLogger(ProductCartServiceImpl.class);
+    private final ProductCartRepository productCartRepository;
+
+    @Autowired
+    public ProductCartServiceImpl(ProductCartRepository productCartRepository) {
+        this.productCartRepository = productCartRepository;
+    }
+
+    @Override
+    public void saveProductCart(Cart cart, Product product) {
+        ProductCart productCart = new ProductCart();
+        productCart.setCart(cart);
+        productCart.setProduct(product);
+        productCartRepository.save(productCart);
+        logger.info("Saved product cart successfully");
+    }
+}
