fbe79d2493dec0f1ef233fa2e92bf6e8eead2dc7
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-25 19:26:08 +0700

    chore: Update CartService and implement it for save method.

--- src/main/java/org/example/coffeeshopwebsite/service/CartService.java
+++ src/main/java/org/example/coffeeshopwebsite/service/CartService.java
@@ -1,13 +1,5 @@
 package org.example.coffeeshopwebsite.service;
 
-import jakarta.servlet.http.HttpSession;
-import org.example.coffeeshopwebsite.model.Product;
-import org.example.coffeeshopwebsite.model.User;
-
-import java.util.List;
-
 public interface CartService {
-    void addProductToCart(Long productId, HttpSession session);
-
-    List<Product> getCartItems(HttpSession session);
+    void addProductToCart(Integer quantity, Long productId);
 }
--- src/main/java/org/example/coffeeshopwebsite/service/CartServiceImpl.java
+++ src/main/java/org/example/coffeeshopwebsite/service/CartServiceImpl.java
@@ -1,68 +1,52 @@
 package org.example.coffeeshopwebsite.service;
 
-import jakarta.servlet.http.HttpSession;
 import org.example.coffeeshopwebsite.model.Cart;
 import org.example.coffeeshopwebsite.model.Product;
 import org.example.coffeeshopwebsite.model.User;
 import org.example.coffeeshopwebsite.repository.CartRepository;
+import org.example.coffeeshopwebsite.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class CartServiceImpl implements CartService{
     private final Logger logger = LoggerFactory.getLogger(CartServiceImpl.class);
     private final CartRepository cartRepository;
     private final UserService userService;
-    private final ProductService productService;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public CartServiceImpl(CartRepository cartRepository, UserService userService, ProductService productService) {
+    public CartServiceImpl(CartRepository cartRepository, UserService userService, ProductRepository productRepository) {
         this.cartRepository = cartRepository;
         this.userService = userService;
-        this.productService = productService;
+        this.productRepository = productRepository;
     }
 
     @Override
-    public void addProductToCart(Long productId, HttpSession session) {
-        try {
-            User user = userService.getCurrentUser();
-            Product product = productService.getProductById(productId);
-            Cart cart = (Cart) session.getAttribute("cart");
-            if (cart == null) {
-                cart = new Cart();
-                cart.setUser(user);
-                cart.setProducts(new ArrayList<>());
-            } else if (cart.getProducts() == null) {
-                cart.setProducts(new ArrayList<>());
-            }
-            cart.setTotalBill(product.getSellingPrice() * product.getQuantity());
-            cart.getProducts().add(product);
-            session.setAttribute("cart", cart);
-            cartRepository.save(cart);
-            logger.info("Saved cart successful");
-        } catch (Exception e) {
-            System.out.println("e = " + e);
-        }
-    }
-
-    @Override
-    public List<Product> getCartItems(HttpSession session) {
+    public void addProductToCart(Integer quantity, Long productId) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
         User user = userService.getCurrentUser();
-        Cart cart = (Cart) session.getAttribute("cart");
-
-        if (cart == null) {
-            cart = cartRepository.findByUser(user);
-            if (cart != null) {
-                session.setAttribute("cart", cart);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            Cart cart = new Cart();
+            cart.setProducts(new ArrayList<>());
+            cart.getProducts().add(product);
+            cart.setUser(user);
+            cart.setQuantity(quantity);
+            cart.setTotalBill(product.getSellingPrice() * quantity);
+            if(product.getCarts() == null) {
+                product.setCarts(new ArrayList<>());
             }
+            product.getCarts().add(cart);
+            cartRepository.save(cart);
+            logger.info("Cart save successfully");
+            productRepository.save(product);
+            logger.info("Product save successfully");
         }
-
-        return cart != null ? cart.getProducts().stream().collect(Collectors.toList()) : null;
     }
 }
