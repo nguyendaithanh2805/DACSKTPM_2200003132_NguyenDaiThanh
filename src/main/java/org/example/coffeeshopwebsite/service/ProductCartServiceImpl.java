83225de3f1f49a3f19f77c036978c602d3449b42
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-26 14:51:35 +0700

    chore: Update ProductCartServiceImpl
    - Update ProductCartServiceImpl so that when a ProductCart is deleted, the Product tables is restored.

--- src/main/java/org/example/coffeeshopwebsite/service/ProductCartServiceImpl.java
+++ src/main/java/org/example/coffeeshopwebsite/service/ProductCartServiceImpl.java
@@ -5,6 +5,7 @@ import org.example.coffeeshopwebsite.model.Product;
 import org.example.coffeeshopwebsite.model.ProductCart;
 import org.example.coffeeshopwebsite.model.User;
 import org.example.coffeeshopwebsite.repository.ProductCartRepository;
+import org.example.coffeeshopwebsite.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,13 @@ public class ProductCartServiceImpl implements ProductCartService {
     private final Logger logger = LoggerFactory.getLogger(ProductCartServiceImpl.class);
     private final ProductCartRepository productCartRepository;
     private final UserService userService;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public ProductCartServiceImpl(ProductCartRepository productCartRepository, UserService userService) {
+    public ProductCartServiceImpl(ProductCartRepository productCartRepository, UserService userService, ProductRepository productRepository) {
         this.productCartRepository = productCartRepository;
         this.userService = userService;
+        this.productRepository = productRepository;
     }
 
     @Override
@@ -42,6 +45,10 @@ public class ProductCartServiceImpl implements ProductCartService {
 
     @Override
     public void deleteProductFromCartById(Long productCartId) {
+        ProductCart productCart = productCartRepository.findById(productCartId).orElseThrow(() -> new IllegalArgumentException("Invalid productCart ID :: " + productCartId));
+        Product product = productCart.getProduct();
+        product.setQuantity(product.getQuantity() + productCart.getQuantity());
+        productRepository.save(product);
         productCartRepository.deleteById(productCartId);
         logger.info("Delete product cart success");
     }
