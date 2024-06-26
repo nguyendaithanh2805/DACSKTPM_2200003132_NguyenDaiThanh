475c15de9062221dd738937f0383c3d1c122166b
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-26 15:28:57 +0700

    del: delete shippingRepository.

--- src/main/java/org/example/coffeeshopwebsite/repository/ShippingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.coffeeshopwebsite.repository;
-
-import org.example.coffeeshopwebsite.model.Shipping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShippingRepository extends JpaRepository<Shipping, Long> {
-}
