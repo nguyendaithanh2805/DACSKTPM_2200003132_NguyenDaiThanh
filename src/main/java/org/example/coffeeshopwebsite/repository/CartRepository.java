b29beb9b96561908d329a09048f0bcef562e4e31
nguyendaithanh2805 <thanhdaiu443@gmail.com>
2024-06-26 13:47:19 +0700

    chore: Update ProductCartRepository and CartRepository
    - Add findAllProductFromCartByUser for ProductCartRepository
    - Delete findByUser method for CartRepository.

--- src/main/java/org/example/coffeeshopwebsite/repository/CartRepository.java
+++ src/main/java/org/example/coffeeshopwebsite/repository/CartRepository.java
@@ -9,5 +9,4 @@ import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    List<Cart> findByUser(User user);
 }
--- src/main/java/org/example/coffeeshopwebsite/repository/ProductCartRepository.java
+++ src/main/java/org/example/coffeeshopwebsite/repository/ProductCartRepository.java
@@ -2,8 +2,18 @@ package org.example.coffeeshopwebsite.repository;
 
 import org.example.coffeeshopwebsite.model.ProductCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductCartRepository extends JpaRepository<ProductCart, Long> {
+    /* Test query in database
+        SELECT * FROM tbl_product_cart pc
+		JOIN tbl_product p ON pc.Id = p.id
+		JOIN tbl_cart c ON pc.cart_id = c.id WHERE c.user_id = 2
+    */
+    @Query("SELECT pc FROM ProductCart pc JOIN pc.cart c WHERE c.user.id = :userId")
+    List<ProductCart> findAllProductFromCartByUser(Long userId);
 }
